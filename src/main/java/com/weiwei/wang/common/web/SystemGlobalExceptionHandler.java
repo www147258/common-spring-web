5fa385320e497e11b57fc15faed5741ef94ee3fc
wangweiwei <wangweiwei@ugreen.com>
2025-06-12 11:44:14 +0800

    feat: 异常处理器增加异常

--- src/main/java/com/weiwei/wang/common/web/SystemGlobalExceptionHandler.java
+++ src/main/java/com/weiwei/wang/common/web/SystemGlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.weiwei.wang.common.web;
 
 import com.weiwei.wang.common.domain.response.CommonResponse;
+import com.weiwei.wang.common.exception.BusinessException;
 import com.weiwei.wang.common.web.domain.response.ExceptionDataResponse;
 import com.weiwei.wang.common.web.enums.ResponseExceptionEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.RequestAttributes;
@@ -20,11 +25,53 @@ public class SystemGlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(SystemGlobalExceptionHandler.class);
 
-    @ExceptionHandler(Exception.class)
-    public CommonResponse<ExceptionDataResponse> handlerException(Exception e) {
 
-        logger.error("handlerException ", e);
 
+    @ExceptionHandler(BusinessException.class)
+    public CommonResponse<ExceptionDataResponse> handlerBusinessException(BusinessException e) {
+        logger.warn("handlerBusinessException ", e);
+        CommonResponse fail = CommonResponse.fail(e);
+        fail.setData(buildExceptionDataResponse(e));
+        return fail;
+    }
+
+    //
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public CommonResponse<ExceptionDataResponse> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        logger.warn("handlerMethodArgumentNotValidException ", e);
+        FieldError fieldError = e.getFieldError();
+        String field = fieldError.getField();
+        String defaultMessage = fieldError.getDefaultMessage();
+
+        String message = ResponseExceptionEnum.HANDLER_METHOD_ARGUMENT_NOT_VALID.getMessage();
+
+
+        CommonResponse fail = CommonResponse.fail(ResponseExceptionEnum.HANDLER_METHOD_ARGUMENT_NOT_VALID);
+        fail.setData(buildExceptionDataResponse(e));
+        fail.setMessage(String.format(message, field, defaultMessage));
+        return fail;
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public CommonResponse<ExceptionDataResponse> handlerHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        logger.warn("handlerHttpMessageNotReadableException ", e);
+        CommonResponse fail = CommonResponse.fail(ResponseExceptionEnum.HTTP_MESSAGE_NOT_READABLE);
+        fail.setData(buildExceptionDataResponse(e));
+        return fail;
+    }
+
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public CommonResponse<ExceptionDataResponse> handlerHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        logger.warn("handlerHttpRequestMethodNotSupportedException ", e);
+        CommonResponse fail = CommonResponse.fail(ResponseExceptionEnum.HTTP_REQUEST_METHOD_NOT_SUPPORTED);
+        fail.setData(buildExceptionDataResponse(e));
+        return fail;
+    }
+
+
+    private ExceptionDataResponse buildExceptionDataResponse(Exception e) {
         ExceptionDataResponse exceptionDataResponse = new ExceptionDataResponse();
 
         RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
@@ -32,21 +79,27 @@ public class SystemGlobalExceptionHandler {
 
         if( requestAttributes instanceof  ServletRequestAttributes) {
             jakarta.servlet.http.HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
-
             urlPath = request.getServletPath();
-
-
-
         }
         exceptionDataResponse.setUrlPath(urlPath);
 
         exceptionDataResponse.setExceptionName(e.getClass().getName());
 
         exceptionDataResponse.setServerName(applicationName);
+        return exceptionDataResponse;
+    }
+
+
+
+    @ExceptionHandler(Exception.class)
+    public CommonResponse<ExceptionDataResponse> handlerException(Exception e) {
+
+        logger.error("handlerException ", e);
 
         CommonResponse fail = CommonResponse.fail(ResponseExceptionEnum.SYSTEM_FAIL);
-        fail.setData(exceptionDataResponse);
+        fail.setData(buildExceptionDataResponse(e));
         return fail;
+
     }
 
     public String getApplicationName() {
--- src/main/java/com/weiwei/wang/common/web/enums/ResponseExceptionEnum.java
+++ src/main/java/com/weiwei/wang/common/web/enums/ResponseExceptionEnum.java
@@ -5,6 +5,11 @@ import com.weiwei.wang.common.exception.ExceptionResponse;
 public enum ResponseExceptionEnum implements ExceptionResponse {
 
     SYSTEM_FAIL(1, "操作失败！！！"),
+    HTTP_REQUEST_METHOD_NOT_SUPPORTED(2, "http请求方法不支持"),
+
+    HTTP_MESSAGE_NOT_READABLE(3, "http请求内容无法读取，可能格式错误，请检查是否有请求参数或者请求参数格式错误"),
+
+    HANDLER_METHOD_ARGUMENT_NOT_VALID(4,"参数校验失败,字段[%s]，原因[%s]"),
     ;
 
 
